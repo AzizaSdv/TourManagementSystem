5467279ddad2dd0ef1ef519244ef0893cfc07c25
AzizaSdv <241162@astanait.edu.kz>
2025-01-29 23:49:24 +0500

    Create Tour.java

---
+++ src/main/java/com/example/tourmanagement/Tour.java
@@ -0,0 +1,27 @@
+public class Tour {
+    private int id;
+    private String name;
+    private String location;
+    private double price;
+    private String dates;
+
+    public Tour(String name, String location, double price, String dates) {
+        this.name = name;
+        this.location = location;
+        this.price = price;
+        this.dates = dates;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public String getLocation() { return location; }
+    public double getPrice() { return price; }
+    public String getDates() { return dates; }
+
+    @Override
+    public String toString() {
+        return "Tour: " + name + ", Location: " + location + ", Price: $" + price + ", Dates: " + dates;
+    }
+}
